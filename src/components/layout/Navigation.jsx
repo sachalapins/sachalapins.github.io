97a9b6cae640dddfd69c75ad5458d3a499b76e9c
sachalapins <sacha.lapins@gmail.com>
2025-01-01 14:12:19 +0000

    Updated navigation button appearance

--- src/components/layout/Navigation.jsx
+++ src/components/layout/Navigation.jsx
@@ -28,7 +28,7 @@ const Navigation = ({ currentPage, setCurrentPage }) => {
                     currentPage === item.id
                       ? 'border-b-2 border-blue-500 text-gray-900'
                       : 'text-gray-500 hover:text-gray-900 hover:border-gray-300'
-                  } inline-flex items-center px-1 pt-1 text-sm font-medium`}
+                  } inline-flex items-center px-1 pt-1 text-base font-medium  leading-relaxed`}
                 >
                   {item.label}
                 </button>
