239f67ea6e8926c91c44b701c055082368e469e3
sachalapins <sacha.lapins@gmail.com>
2025-01-01 15:12:14 +0000

    Fixed build error

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ExternalLink, ArrowRight } from 'lucide-react';
 import { newsItems } from '../../data/news';
 import profilePhoto from '../../data/headshot_orange.jpg';
